dd0c1d1e8aac797900aed022710fc01cc74104be
Joseph Daramola <daramolaj@gmail.com>
2019-09-25 21:38:02 +0100

    completed gitfinder app...project 1 (React-front-to-back)

---
+++ src/components/users/Search.js
@@ -0,0 +1,46 @@
+import React, { useState, useContext } from 'react';
+import AlertContext from '../../context/alert/alertContext';
+import GithubContext from '../../context/github/githubContext';
+
+const Search = () => {
+	const alertContext = useContext(AlertContext);
+	const githubContext = useContext(GithubContext);
+
+	const [text, setText] = useState(' ');
+
+	const onSubmit = e => {
+		e.preventDefault();
+		if (text === '') {
+			alertContext.setAlert(' Please enter something', 'light');
+		} else {
+			githubContext.searchUsers(text);
+			setText('');
+		}
+	};
+
+	const onChange = e => setText(e.target.value);
+
+	return (
+		<div>
+			<form className='form' onSubmit={onSubmit}>
+				<input
+					type='text'
+					name='text'
+					placeholder='Search Users...'
+					value={text}
+					onChange={onChange}
+				/>
+				<input type='submit' value='Search' className='btn btn-dark btn-block' />
+			</form>
+			{githubContext.users.length > 0 && (
+				<button
+					className='btn btn-light btn-block'
+					onClick={githubContext.clearUsers}>
+					Clear
+				</button>
+			)}
+		</div>
+	);
+};
+
+export default Search;
